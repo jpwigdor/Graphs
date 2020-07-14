4abd0b1c86a28abb938a4c34c17b01781486581e
James Wigdor <jpwigdor@gmail.com>
2020-07-14 16:35:01 -0400

    completed Day 1 MVP

--- projects/graph/util.py
+++ projects/graph/util.py
@@ -3,26 +3,33 @@
 class Queue():
     def __init__(self):
         self.queue = []
+
     def enqueue(self, value):
         self.queue.append(value)
+
     def dequeue(self):
         if self.size() > 0:
+            # this deletes the first index and it has to move every item over 1 index. a better implimentation would be a LinkedList.
             return self.queue.pop(0)
         else:
             return None
+
     def size(self):
         return len(self.queue)
+
 
 class Stack():
     def __init__(self):
         self.stack = []
+
     def push(self, value):
         self.stack.append(value)
+
     def pop(self):
         if self.size() > 0:
             return self.stack.pop()
         else:
             return None
+
     def size(self):
         return len(self.stack)
-
