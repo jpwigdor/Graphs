718425f41b032fd15af04d306a1b4c50f4a2142e
James Wigdor <jpwigdor@gmail.com>
2020-07-17 12:24:21 -0400

    Graph SC complete

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,7 +1,7 @@
 from room import Room
 from player import Player
 from world import World
-
+from util import Stack
 import random
 from ast import literal_eval
 
@@ -17,21 +17,75 @@ world = World()
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
 world.print_rooms()
 
 player = Player(world.starting_room)
-
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
+
+
+def reverse_exit(direction):
+    if direction == 'n':
+        return 's'
+    elif direction == 'e':
+        return 'w'
+    elif direction == 's':
+        return 'n'
+    elif direction == 'w':
+        return 'e'
+
+
 traversal_path = []
+path = []
+visited = {}
+count = 0
+cur_room = player.current_room
+visited[player.current_room.id] = player.current_room.get_exits()
+print(f'starting dict: {visited}')
+print(f'length of dict: {len(visited)}')
+print(f'path: {path}')
+
+# Loop while there are rooms being passed into the stack
+while len(visited) < len(room_graph) - 1:
+    count += 1
+    moves = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
+    if player.current_room.id not in visited:
+        # mark room as visisted
+        # populate known rooms into a hash table
+        visited[player.current_room.id] = player.current_room.get_exits()
+        print(
+            f'current room: {player.current_room.id}: \nexists {visited[player.current_room.id]}')
+        # track previous
+        prev = path[-1]
+
+        # print(f'visited: {visited[player.current_room.id]}')
+        # print(f'current room: {player.current_room.id}')
+        # print(f'previous: {prev}')
+        visited[player.current_room.id].remove(prev)
+        # loop through room's exists
+
+    while len(visited[player.current_room.id]) == 0:
+        # back track
+        prev = path.pop()
+        # record the move back
+        traversal_path.append(prev)
+        player.travel(prev)
+
+    next_room = visited[player.current_room.id].pop(-1)
+    # add next move to our tracking path
+    path.append(moves[next_room])
+    print(f'adding to path: {moves[next_room]}')
+    # add to the instructions path
+    traversal_path.append(next_room)
+    # move the player
+    player.travel(next_room)
 
 
-
-# TRAVERSAL TEST
+# TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
@@ -41,11 +95,11 @@ for move in traversal_path:
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
 
 
 #######
